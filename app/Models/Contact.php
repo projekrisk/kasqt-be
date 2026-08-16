5dc54d74822c9ab1dc2a820e434dec260ca07884
Projekrisk <projekrisk@gmail.com>
2026-08-16 18:19:31 +0700

    Kontak

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -9,6 +9,14 @@ class Contact extends Model
 {
     protected $fillable = ['user_id', 'name', 'phone_number', 'is_kasqt_user'];
 
+    // FITUR BARU: Memaksa MySQL menerjemahkan 0/1 menjadi true/false untuk Android
+    protected function casts(): array
+    {
+        return [
+            'is_kasqt_user' => 'boolean',
+        ];
+    }
+
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
