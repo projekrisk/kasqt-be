fc1e11a44ddf828dcf0eedbe073e1ea01651d744
Projekrisk <projekrisk@gmail.com>
2026-08-23 20:14:25 +0700

    navbar pengguna

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -13,10 +13,12 @@ use Filament\Tables\Table;
 class UserResource extends Resource
 {
     protected static ?string $model = User::class;
-
     protected static ?string $navigationIcon = 'heroicon-o-users';
-    
-    protected static ?string $navigationGroup = 'Manajemen Pengguna';
+    protected static ?string $modelLabel = 'Pengguna';
+    protected static ?string $navigationGroup = 'Manajemen';
+    protected static ?string $pluralModelLabel = 'Pengguna';
+    protected static ?string $navigationLabel = 'Pengguna';
+    protected static ?string $slug = 'pengguna';    
 
     public static function form(Form $form): Form
     {
