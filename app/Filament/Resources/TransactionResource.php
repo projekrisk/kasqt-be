e8dc439f21710f412d8fa0dbaa695a62cb3d40f5
Projekrisk <projekrisk@gmail.com>
2026-08-25 00:11:09 +0700

    widget transaksi

--- app/Filament/Resources/TransactionResource.php
+++ app/Filament/Resources/TransactionResource.php
@@ -13,10 +13,12 @@ use Filament\Tables\Table;
 class TransactionResource extends Resource
 {
     protected static ?string $model = Transaction::class;
-
     protected static ?string $navigationIcon = 'heroicon-o-banknotes';
-    
-    protected static ?string $navigationGroup = 'Transaksi';
+    protected static ?string $modelLabel = 'Transaksi';
+    protected static ?string $navigationGroup = 'Manajemen';
+    protected static ?string $pluralModelLabel = 'Transaksi';
+    protected static ?string $navigationLabel = 'Transaksi';
+    protected static ?string $slug = 'transaksi';
 
     public static function form(Form $form): Form
     {
